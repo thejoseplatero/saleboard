7a482e02e4750a0236576aae465c20bb93577b77
Jose Platero <contact@joseplatero.com>
2013-03-21 23:01:32 -0400

    added brand routing

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-  attr_accessible :description, :name
-  validates :description, :name, presence: true
+  attr_accessible :description, :name, :price
+  validates :description, :name, :price, presence: true
   #validates :user, presence: true
 
   has_many :comments
