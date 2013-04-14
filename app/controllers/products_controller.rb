792008451faf33852e6e8630805ce563cadd3a41
Jose Platero <contact@joseplatero.com>
2013-04-14 13:59:53 -0400

    fixed edit routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,6 +13,20 @@ skip_before_filter :authenticate_user!, only: [:index]
 
 	end
 
+	def edit 
+		@product = Product.find(params[:id])
+	end
+
+	def update
+		@product = Product.find(params[:id])
+			if @product.update_attributes params[:product]
+				redirect_to products_path
+			else
+				render :edit
+			end
+	end
+
+
 	def new
 		@product = Product.new
 	end
